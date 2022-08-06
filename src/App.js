19940fd4302103adef6d7ff5838695fc3df1b9f9
mohamed aziz ammar <azizammar67@gmail.com>
2022-08-06 16:09:50 +0100

    addding the fuul file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./componeds/navbar/Navbar";
+import Bt from "./componeds/bootsss/Bt";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Bt/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componeds/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import "./navbar.css"
+const Navbar = () => {
+  return (
+    <div>
+        <nav className="nav">
+      <a bref="/" class="av-link">Home</a>
+      <a href="/products/" class="nav-link">Products</a>
+      <a href="/about/" class="nav-link">About</a>
+      <a href="/blog/" class="nav-link">Blog</a>
+    </nav>
+<br />
+<br />
+<h1 class="ha">React-JS</h1>
+<p>Lorem ipsum dolor sit amet consectetur
+   adipisicing elit. Ab, atque excepturi quae eos,
+   deleniti nostrum cum reiciendis
+   dolorum quod, saepe est quam architecto
+    nulla hic. Deserunt necessitatibus numquam veritatis est.</p>
+    <br />
+    <div className="sas">
+    <br/>
+    <br/>
+    <br/>
+    </div>
+    <br />
+    </div>
+  )
+  }
+
+
+export default Navbar
